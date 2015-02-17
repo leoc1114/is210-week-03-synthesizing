896cfac5dc9fc67b049558a559ee5f2b86beffd4
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-17 01:58:38 +0000

    modified task 5

--- task_05.py
+++ task_05.py
@@ -61,10 +61,10 @@ def is_empty(my_sequence):
     """
     count = get_member_count(my_sequence)
 
-    if count != False:
-        return count == 0
+    if count != 0:
+        return count == True
     else:
-        raise TypeError('Object has no len()')
+        return count == False
 
 
 TEST = ''
